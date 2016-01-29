d9b28a4d7e529f8cafcbb229f6db6e2a7a98d81b
Karl Stenerud <kstenerud@gmail.com>
2016-01-28 16:45:06 -0800

    Add basic swift crash tester.

---
+++ iOS/Swift-Tester/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Swift-Tester
+//
+//  Created by karl on 2016-01-28.
+//  Copyright © 2016 Karl Stenerud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func doSomething() {
+        doSomething()
+    }
+
+    @IBAction func onButtonPressed(sender: AnyObject) {
+        doSomething()
+    }
+}
+
