4cf81a377097ec88b4146a7da07cc00924a8ff31
Karl Stenerud <kstenerud@gmail.com>
2016-04-25 11:47:29 -0700

    Give __cxa_throw weak linkage so that at the very least it won't cause a linker error in case of a collision. Note that this will also cause c++ exceptions to not always generate a useful stack trace if there's another overridden __cxa_throw.

--- Source/KSCrash/Recording/Sentry/KSCrashSentry_CPPException.mm
+++ Source/KSCrash/Recording/Sentry/KSCrashSentry_CPPException.mm
@@ -77,22 +77,26 @@ static KSCrash_SentryContext* g_context;
 
 typedef void (*cxa_throw_type)(void*, std::type_info*, void (*)(void*));
 
-extern "C" void __cxa_throw(void* thrown_exception, std::type_info* tinfo, void (*dest)(void*))
+extern "C"
 {
-    if(g_captureNextStackTrace)
-    {
-        g_stackTraceCount = backtrace((void**)g_stackTrace, sizeof(g_stackTrace) / sizeof(*g_stackTrace));
-    }
+    void __cxa_throw(void* thrown_exception, std::type_info* tinfo, void (*dest)(void*)) __attribute__ ((weak));
 
-    static cxa_throw_type orig_cxa_throw = NULL;
-    unlikely_if(orig_cxa_throw == NULL)
+    void __cxa_throw(void* thrown_exception, std::type_info* tinfo, void (*dest)(void*))
     {
-        orig_cxa_throw = (cxa_throw_type) dlsym(RTLD_NEXT, "__cxa_throw");
+        if(g_captureNextStackTrace)
+        {
+            g_stackTraceCount = backtrace((void**)g_stackTrace, sizeof(g_stackTrace) / sizeof(*g_stackTrace));
+        }
+        
+        static cxa_throw_type orig_cxa_throw = NULL;
+        unlikely_if(orig_cxa_throw == NULL)
+        {
+            orig_cxa_throw = (cxa_throw_type) dlsym(RTLD_NEXT, "__cxa_throw");
+        }
+        orig_cxa_throw(thrown_exception, tinfo, dest);
+        __builtin_unreachable();
     }
-    orig_cxa_throw(thrown_exception, tinfo, dest);
-    __builtin_unreachable();
 }
-
 
 static void CPPExceptionTerminate(void)
 {
