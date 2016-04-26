01af90520f2e5e50f131d3a32efd640189b0ae57
Karl Stenerud <kstenerud@gmail.com>
2016-04-26 14:09:33 -0700

    Increment version

--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -533,7 +533,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.52;
+const double KSCrashFrameworkVersionNumber = 1.53;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.5.2";
+const unsigned char KSCrashFrameworkVersionString[] = "1.5.3";
