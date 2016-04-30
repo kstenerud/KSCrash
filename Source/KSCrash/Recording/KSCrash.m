0663d4b5a6d22a0a5fa271b7c6ab4a33baef6904
Karl Stenerud <kstenerud@gmail.com>
2016-04-30 09:21:55 -0700

    Bump version

--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -533,7 +533,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.53;
+const double KSCrashFrameworkVersionNumber = 1.54;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.5.3";
+const unsigned char KSCrashFrameworkVersionString[] = "1.5.4";
