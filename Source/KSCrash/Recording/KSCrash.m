dfd01aff24f796569e8b1e00ae794598462e172b
Karl Stenerud <kstenerud@gmail.com>
2016-11-22 12:45:16 -0800

    Bump to 1.10.3 because cocoapods is annoying

--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -572,7 +572,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.102;
+const double KSCrashFrameworkVersionNumber = 1.103;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.10.2";
+const unsigned char KSCrashFrameworkVersionString[] = "1.10.3";
