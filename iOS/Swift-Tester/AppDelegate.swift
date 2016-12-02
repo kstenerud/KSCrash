7014ef2189d53d3c48b7a8d47e4e69865f360c0f
Karl Stenerud <kstenerud@gmail.com>
2016-12-02 11:44:12 -0800

    Update swift example.

--- iOS/Swift-Tester/AppDelegate.swift
+++ iOS/Swift-Tester/AppDelegate.swift
@@ -15,27 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let emailAddress = "your@email.here"
         
         let installation = KSCrashInstallationEmail.sharedInstance()
-        installation.recipients = [emailAddress]
-        installation.subject = "Crash Report"
-        installation.message = "This is a crash report"
-        installation.filenameFmt = "crash-report-%d.json.gz"
+        installation?.recipients = [emailAddress]
+        installation?.subject = "Crash Report"
+        installation?.message = "This is a crash report"
+        installation?.filenameFmt = "crash-report-%d.json.gz"
 //        installation.reportStyle = KSCrashEmailReportStyleJSON
         
-        installation.addConditionalAlertWithTitle("Crash Detected",
+        installation?.addConditionalAlert(withTitle: "Crash Detected",
             message: "The app crashed last time it was launched. Send a crash report?",
             yesAnswer: "Sure!",
             noAnswer: "No thanks")
 
-        installation.install()
+        installation?.install()
 
-        installation.sendAllReportsWithCompletion { (reports, completed, error) -> Void in
+        installation?.sendAllReports { (reports, completed, error) -> Void in
             if(completed) {
-                print("Sent \(reports.count) reports")
+                print("Sent \(reports?.count) reports")
             } else {
                 print("Failed to send reports: \(error)")
             }
@@ -44,25 +44,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
--- iOS/Swift-Tester/ViewController.swift
+++ iOS/Swift-Tester/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func onButtonPressed(sender: AnyObject) {
+    @IBAction func onButtonPressed(_ sender: AnyObject) {
         var emptyDictionary = Dictionary<Int,Int>()
         print(emptyDictionary[1]!)
     }
