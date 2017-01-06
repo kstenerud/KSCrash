7761fc0f3b3a3e4e13565e461c5a3878b231ec0e
Karl Stenerud <kstenerud@gmail.com>
2017-01-06 13:49:01 -0800

    Remove cruft

--- Android/app/src/main/java/org/stenerud/kscrash/MainActivity.java
+++ Android/app/src/main/java/org/stenerud/kscrash/MainActivity.java
@@ -29,6 +29,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
     public native String stringFromTimestamp(long timestamp);
 }
