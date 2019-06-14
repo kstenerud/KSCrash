c63d42bec08c2c9859537f442cdfbc6a653fb3c2
Nikolay Volosatov <bamx23@yandex-team.ru>
2019-06-14 13:05:19 +0300

    Release 1.15.21

--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -639,7 +639,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.1520;
+const double KSCrashFrameworkVersionNumber = 1.1521;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.15.20";
+const unsigned char KSCrashFrameworkVersionString[] = "1.15.21";
