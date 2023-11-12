d4dd08ee07db94bac5faa071a45ffda5b223f9f0
Nikolay Volosatov <code@bamx23.com>
2023-11-12 16:29:08 +0000

    Bump version 1.16.0

--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -649,7 +649,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.1527;
+const double KSCrashFrameworkVersionNumber = 1.1600;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.15.27";
+const unsigned char KSCrashFrameworkVersionString[] = "1.16.0";
