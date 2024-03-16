8e29ada12998c47b1ca595bb21d47a27f4e57b22
Nikolay Volosatov <code@bamx23.com>
2024-03-16 11:39:36 +0000

    Bump version 1.17.0

--- KSCrash.podspec
+++ KSCrash.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
   IOS_DEPLOYMENT_TARGET = '11.0' unless defined? IOS_DEPLOYMENT_TARGET
   s.name         = "KSCrash"
-  s.version      = "1.16.2"
+  s.version      = "1.17.0"
   s.summary      = "The Ultimate iOS Crash Reporter"
   s.homepage     = "https://github.com/kstenerud/KSCrash"
   s.license     = { :type => 'KSCrash license agreement', :file => 'LICENSE' }
--- Source/KSCrash/Recording/KSCrash.m
+++ Source/KSCrash/Recording/KSCrash.m
@@ -649,7 +649,7 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 
 
 //! Project version number for KSCrashFramework.
-const double KSCrashFrameworkVersionNumber = 1.1602;
+const double KSCrashFrameworkVersionNumber = 1.1700;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "1.16.2";
+const unsigned char KSCrashFrameworkVersionString[] = "1.17.0";
