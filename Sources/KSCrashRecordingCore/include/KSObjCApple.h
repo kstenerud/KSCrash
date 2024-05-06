7866a7af5945267ebe626624aee711b5929b1bef
Gleb Linnik <glinnik@yandex-team.ru>
2024-05-06 18:54:44 +0300

    Update KSObjCApple.h with latest Obj-C sources

--- Sources/KSCrashRecordingCore/include/KSObjCApple.h
+++ Sources/KSCrashRecordingCore/include/KSObjCApple.h
@@ -59,56 +59,28 @@ NAME { \
 
 
 // ======================================================================
-#pragma mark - objc4-680/runtime/objc-config.h -
+#pragma mark - objc4-912.3/runtime/objc-config.h -
 // ======================================================================
 
 // Define SUPPORT_TAGGED_POINTERS=1 to enable tagged pointer objects
 // Be sure to edit tagged pointer SPI in objc-internal.h as well.
-#if !(__LP64__)
+#if !__LP64__
 #   define SUPPORT_TAGGED_POINTERS 0
 #else
 #   define SUPPORT_TAGGED_POINTERS 1
 #endif
-    
+
 // Define SUPPORT_MSB_TAGGED_POINTERS to use the MSB
 // as the tagged pointer marker instead of the LSB.
 // Be sure to edit tagged pointer SPI in objc-internal.h as well.
-#if !SUPPORT_TAGGED_POINTERS  ||  !TARGET_OS_IPHONE
+#if !SUPPORT_TAGGED_POINTERS  ||  ((TARGET_OS_OSX || TARGET_OS_MACCATALYST) && __x86_64__)
 #   define SUPPORT_MSB_TAGGED_POINTERS 0
 #else
 #   define SUPPORT_MSB_TAGGED_POINTERS 1
 #endif
 
-
 // ======================================================================
-#pragma mark - objc4-680/runtime/objc-object.h -
-// ======================================================================
-    
-#if SUPPORT_TAGGED_POINTERS
-
-// KS: The original values wouldn't have worked. The slot shift and mask
-// were incorrect.
-#define TAG_COUNT 8
-//#define TAG_SLOT_MASK 0xf
-#define TAG_SLOT_MASK 0x07
-
-#if SUPPORT_MSB_TAGGED_POINTERS
-#   define TAG_MASK (1ULL<<63)
-#   define TAG_SLOT_SHIFT 60
-#   define TAG_PAYLOAD_LSHIFT 4
-#   define TAG_PAYLOAD_RSHIFT 4
-#else
-#   define TAG_MASK 1
-//#   define TAG_SLOT_SHIFT 0
-#   define TAG_SLOT_SHIFT 1
-#   define TAG_PAYLOAD_LSHIFT 0
-#   define TAG_PAYLOAD_RSHIFT 4
-#endif
-
-#endif
-
-// ======================================================================
-#pragma mark - objc4-781/runtime/objc-internal.h -
+#pragma mark - objc4-912.3/runtime/objc-config.h -
 // ======================================================================
 #if __ARM_ARCH_7K__ >= 2  ||  (__arm64__ && !__LP64__)
 #   define SUPPORT_INDEXED_ISA 1
@@ -117,11 +89,17 @@ NAME { \
 #endif
 
 // ======================================================================
-#pragma mark - objc4-680/runtime/objc-internal.h -
+#pragma mark - objc4-912.3/runtime/objc-internal.h -
 // ======================================================================
 
+#if __has_feature(objc_fixed_enum)  ||  __cplusplus >= 201103L
+enum objc_tag_index_t : uint16_t
+#else
+typedef uint16_t objc_tag_index_t;
 enum
+#endif
 {
+    // 60-bit payloads
     OBJC_TAG_NSAtom            = 0,
     OBJC_TAG_1                 = 1,
     OBJC_TAG_NSString          = 2,
@@ -129,10 +107,215 @@ enum
     OBJC_TAG_NSIndexPath       = 4,
     OBJC_TAG_NSManagedObjectID = 5,
     OBJC_TAG_NSDate            = 6,
-    OBJC_TAG_7                 = 7
+
+    // 60-bit reserved
+    OBJC_TAG_RESERVED_7        = 7,
+
+    // 52-bit payloads
+    OBJC_TAG_Photos_1          = 8,
+    OBJC_TAG_Photos_2          = 9,
+    OBJC_TAG_Photos_3          = 10,
+    OBJC_TAG_Photos_4          = 11,
+    OBJC_TAG_XPC_1             = 12,
+    OBJC_TAG_XPC_2             = 13,
+    OBJC_TAG_XPC_3             = 14,
+    OBJC_TAG_XPC_4             = 15,
+    OBJC_TAG_NSColor           = 16,
+    OBJC_TAG_UIColor           = 17,
+    OBJC_TAG_CGColor           = 18,
+    OBJC_TAG_NSIndexSet        = 19,
+    OBJC_TAG_NSMethodSignature = 20,
+    OBJC_TAG_UTTypeRecord      = 21,
+    OBJC_TAG_Foundation_1      = 22,
+    OBJC_TAG_Foundation_2      = 23,
+    OBJC_TAG_Foundation_3      = 24,
+    OBJC_TAG_Foundation_4      = 25,
+    OBJC_TAG_CGRegion          = 26,
+
+    // When using the split tagged pointer representation
+    // (OBJC_SPLIT_TAGGED_POINTERS), this is the first tag where
+    // the tag and payload are unobfuscated. All tags from here to
+    // OBJC_TAG_Last52BitPayload are unobfuscated. The shared cache
+    // builder is able to construct these as long as the low bit is
+    // not set (i.e. even-numbered tags).
+    OBJC_TAG_FirstUnobfuscatedSplitTag = 136, // 128 + 8, first ext tag with high bit set
+
+    OBJC_TAG_Constant_CFString = 136,
+
+    OBJC_TAG_First60BitPayload = 0,
+    OBJC_TAG_Last60BitPayload  = 6,
+    OBJC_TAG_First52BitPayload = 8,
+    OBJC_TAG_Last52BitPayload  = 263,
+
+    OBJC_TAG_RESERVED_264      = 264
 };
+#if __has_feature(objc_fixed_enum)  &&  !defined(__cplusplus)
+typedef enum objc_tag_index_t objc_tag_index_t;
+#endif
 
+#if __arm64__
+// ARM64 uses a new tagged pointer scheme where normal tags are in
+// the low bits, extended tags are in the high bits, and half of the
+// extended tag space is reserved for unobfuscated payloads.
+#   define OBJC_SPLIT_TAGGED_POINTERS 1
+#else
+#   define OBJC_SPLIT_TAGGED_POINTERS 0
+#endif
 
+#if (TARGET_OS_OSX || TARGET_OS_MACCATALYST) && __x86_64__
+// 64-bit Mac - tag bit is LSB
+#   define OBJC_MSB_TAGGED_POINTERS 0
+#else
+// Everything else - tag bit is MSB
+#   define OBJC_MSB_TAGGED_POINTERS 1
+#endif
+
+#define _OBJC_TAG_INDEX_MASK 0x7UL
+
+#if OBJC_SPLIT_TAGGED_POINTERS
+#define _OBJC_TAG_SLOT_COUNT 8
+#define _OBJC_TAG_SLOT_MASK 0x7UL
+#else
+// array slot includes the tag bit itself
+#define _OBJC_TAG_SLOT_COUNT 16
+#define _OBJC_TAG_SLOT_MASK 0xfUL
+#endif
+
+#define _OBJC_TAG_EXT_INDEX_MASK 0xff
+
+#if OBJC_SPLIT_TAGGED_POINTERS
+#   define _OBJC_TAG_MASK (1UL<<63)
+#   define _OBJC_TAG_INDEX_SHIFT 0
+#   define _OBJC_TAG_SLOT_SHIFT 0
+#   define _OBJC_TAG_PAYLOAD_LSHIFT 1
+#   define _OBJC_TAG_PAYLOAD_RSHIFT 4
+#   define _OBJC_TAG_EXT_MASK (_OBJC_TAG_MASK | 0x7UL)
+#   define _OBJC_TAG_NO_OBFUSCATION_MASK ((1UL<<62) | _OBJC_TAG_EXT_MASK)
+#   define _OBJC_TAG_CONSTANT_POINTER_MASK \
+~(_OBJC_TAG_EXT_MASK | ((uintptr_t)_OBJC_TAG_EXT_SLOT_MASK << _OBJC_TAG_EXT_SLOT_SHIFT))
+#   define _OBJC_TAG_EXT_INDEX_SHIFT 55
+#   define _OBJC_TAG_EXT_SLOT_SHIFT 55
+#   define _OBJC_TAG_EXT_PAYLOAD_LSHIFT 9
+#   define _OBJC_TAG_EXT_PAYLOAD_RSHIFT 12
+#elif OBJC_MSB_TAGGED_POINTERS
+#   define _OBJC_TAG_MASK (1UL<<63)
+#   define _OBJC_TAG_INDEX_SHIFT 60
+#   define _OBJC_TAG_SLOT_SHIFT 60
+#   define _OBJC_TAG_PAYLOAD_LSHIFT 4
+#   define _OBJC_TAG_PAYLOAD_RSHIFT 4
+#   define _OBJC_TAG_EXT_MASK (0xfUL<<60)
+#   define _OBJC_TAG_EXT_INDEX_SHIFT 52
+#   define _OBJC_TAG_EXT_SLOT_SHIFT 52
+#   define _OBJC_TAG_EXT_PAYLOAD_LSHIFT 12
+#   define _OBJC_TAG_EXT_PAYLOAD_RSHIFT 12
+#else
+#   define _OBJC_TAG_MASK 1UL
+#   define _OBJC_TAG_INDEX_SHIFT 1
+#   define _OBJC_TAG_SLOT_SHIFT 0
+#   define _OBJC_TAG_PAYLOAD_LSHIFT 0
+#   define _OBJC_TAG_PAYLOAD_RSHIFT 4
+#   define _OBJC_TAG_EXT_MASK 0xfUL
+#   define _OBJC_TAG_EXT_INDEX_SHIFT 4
+#   define _OBJC_TAG_EXT_SLOT_SHIFT 4
+#   define _OBJC_TAG_EXT_PAYLOAD_LSHIFT 0
+#   define _OBJC_TAG_EXT_PAYLOAD_RSHIFT 12
+#endif
+
+static inline bool
+_objc_isTaggedPointer(const void * _Nullable ptr)
+{
+    return ((uintptr_t)ptr & _OBJC_TAG_MASK) == _OBJC_TAG_MASK;
+}
+
+#if OBJC_SPLIT_TAGGED_POINTERS
+extern uintptr_t objc_debug_taggedpointer_obfuscator;
+# else
+uintptr_t objc_debug_taggedpointer_obfuscator = 0
+#endif
+
+static inline uintptr_t
+_objc_decodeTaggedPointer_noPermute_withObfuscator(const void * _Nullable ptr,
+                                                   uintptr_t obfuscator)
+{
+    uintptr_t value = (uintptr_t)ptr;
+#if OBJC_SPLIT_TAGGED_POINTERS
+    if ((value & _OBJC_TAG_NO_OBFUSCATION_MASK) == _OBJC_TAG_NO_OBFUSCATION_MASK)
+        return value;
+#endif
+    return value ^ obfuscator;
+}
+
+#if OBJC_SPLIT_TAGGED_POINTERS
+extern uint8_t objc_debug_tag60_permutations[8];
+
+static inline uintptr_t _objc_basicTagToObfuscatedTag(uintptr_t tag) {
+    return objc_debug_tag60_permutations[tag];
+}
+
+static inline uintptr_t _objc_obfuscatedTagToBasicTag(uintptr_t tag) {
+    for (unsigned i = 0; i < 7; i++)
+        if (objc_debug_tag60_permutations[i] == tag)
+            return i;
+    return 7;
+}
+#endif
+
+static inline uintptr_t
+_objc_decodeTaggedPointer_withObfuscator(const void * _Nullable ptr,
+                                         uintptr_t obfuscator)
+{
+    uintptr_t value
+    = _objc_decodeTaggedPointer_noPermute_withObfuscator(ptr, obfuscator);
+#if OBJC_SPLIT_TAGGED_POINTERS
+    uintptr_t basicTag = (value >> _OBJC_TAG_INDEX_SHIFT) & _OBJC_TAG_INDEX_MASK;
+
+    value &= ~(_OBJC_TAG_INDEX_MASK << _OBJC_TAG_INDEX_SHIFT);
+    value |= _objc_obfuscatedTagToBasicTag(basicTag) << _OBJC_TAG_INDEX_SHIFT;
+#endif
+    return value;
+}
+
+static inline objc_tag_index_t
+_objc_getTaggedPointerTag_withObfuscator(const void * _Nullable ptr,
+                                         uintptr_t obfuscator)
+{
+    // ASSERT(_objc_isTaggedPointer(ptr));
+    uintptr_t value = _objc_decodeTaggedPointer_withObfuscator(ptr, obfuscator);
+    uintptr_t basicTag = (value >> _OBJC_TAG_INDEX_SHIFT) & _OBJC_TAG_INDEX_MASK;
+    uintptr_t extTag =   (value >> _OBJC_TAG_EXT_INDEX_SHIFT) & _OBJC_TAG_EXT_INDEX_MASK;
+    if (basicTag == _OBJC_TAG_INDEX_MASK) {
+        return (objc_tag_index_t)(extTag + OBJC_TAG_First52BitPayload);
+    } else {
+        return (objc_tag_index_t)basicTag;
+    }
+}
+
+static inline objc_tag_index_t
+_objc_getTaggedPointerTag(const void * _Nullable ptr)
+{
+    return _objc_getTaggedPointerTag_withObfuscator(ptr, objc_debug_taggedpointer_obfuscator);
+}
+
+__attribute__((no_sanitize("unsigned-shift-base")))
+static inline uintptr_t
+_objc_getTaggedPointerValue_withObfuscator(const void * _Nullable ptr,
+                                           uintptr_t obfuscator)
+{
+    // ASSERT(_objc_isTaggedPointer(ptr));
+    uintptr_t value = _objc_decodeTaggedPointer_noPermute_withObfuscator(ptr, obfuscator);
+    uintptr_t basicTag = (value >> _OBJC_TAG_INDEX_SHIFT) & _OBJC_TAG_INDEX_MASK;
+    if (basicTag == _OBJC_TAG_INDEX_MASK) {
+        return (value << _OBJC_TAG_EXT_PAYLOAD_LSHIFT) >> _OBJC_TAG_EXT_PAYLOAD_RSHIFT;
+    } else {
+        return (value << _OBJC_TAG_PAYLOAD_LSHIFT) >> _OBJC_TAG_PAYLOAD_RSHIFT;
+    }
+}
+
+static inline uintptr_t
+_objc_getTaggedPointerValue(const void * _Nullable ptr)
+{
+    return _objc_getTaggedPointerValue_withObfuscator(ptr, objc_debug_taggedpointer_obfuscator);
+}
 
 // ======================================================================
 #pragma mark - objc4-680/runtime/objc-os.h -
