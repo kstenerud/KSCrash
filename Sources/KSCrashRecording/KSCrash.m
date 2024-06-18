b88a8decac5fa505de9ed2e50052338e55efabc2
Gleb Linnik <glinnik@yandex-team.ru>
2024-06-18 23:43:23 +0200

    Revert "Update version to 2.0.0-alpha.2"
    This reverts commit 1fc6004ac0749f893b1c257bfc516d2bfa8d6224.

--- Sources/KSCrashRecording/KSCrash.m
+++ Sources/KSCrashRecording/KSCrash.m
@@ -565,4 +565,4 @@ SYNTHESIZE_CRASH_STATE_PROPERTY(BOOL, crashedLastLaunch)
 const double KSCrashFrameworkVersionNumber = 2.0000;
 
 //! Project version string for KSCrashFramework.
-const unsigned char KSCrashFrameworkVersionString[] = "2.0.0-alpha.2";
+const unsigned char KSCrashFrameworkVersionString[] = "2.0.0";
