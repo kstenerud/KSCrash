fbc22186b9cb6b955062d43186e562e61a866419
Nikolay Volosatov <code@bamx23.com>
2024-06-23 11:34:00 +0100

    Add headers

--- Samples/Common/Sources/LibraryBridge/ReportingSample.swift
+++ Samples/Common/Sources/LibraryBridge/ReportingSample.swift
@@ -1,3 +1,29 @@
+//
+//  ReportingSample.swift
+//
+//  Created by Nikolay Volosatov on 2024-06-23.
+//
+//  Copyright (c) 2012 Karl Stenerud. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
 import Foundation
 import KSCrashRecording
 import KSCrashFilters
@@ -5,10 +31,9 @@ import KSCrashSinks
 
 public class ReportingSample {
     public static func logToConsole() {
-        KSCrash.sharedInstance().sink = KSCrashReportSinkConsole.filter().defaultCrashReportFilterSet()
-        KSCrash.sharedInstance().sendAllReports { _, _, _ in
-            print("Done!")
-            KSCrash.sharedInstance().deleteAllReports()
+        KSCrash.shared().sink = CrashReportSinkConsole.filter().defaultCrashReportFilterSet()
+        KSCrash.shared().sendAllReports { _, _, _ in
+            print("All reports are sent")
         }
     }
 }
