951eec7e8e8b6483e7cea8ade53aca121140f711
Gleb Linnik <glinnik@yandex-team.ru>
2024-08-30 18:36:28 +0300

    Update version format

--- Sources/KSCrashRecording/KSCrashReportVersion.h
+++ Sources/KSCrashRecording/KSCrashReportVersion.h
@@ -27,6 +27,6 @@
 #ifndef HDR_KSCrashReportVersion_h
 #define HDR_KSCrashReportVersion_h
 
-#define KSCRASH_REPORT_VERSION "3.4.0"
+#define KSCRASH_REPORT_VERSION "3.4.1"
 
 #endif /* HDR_KSCrashReportVersion_h */
