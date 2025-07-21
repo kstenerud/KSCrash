4951b95a7b6fe0f7b3ce8892406baa94b7c6605f
Alexander Cohen <naftaly@users.noreply.github.com>
2025-07-21 11:27:34 -0400

    Update KSCPU_arm64.c

--- Sources/KSCrashRecordingCore/KSCPU_arm64.c
+++ Sources/KSCrashRecordingCore/KSCPU_arm64.c
@@ -41,7 +41,7 @@
 static const char *g_registerNames[] = { "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",  "x8",
                                          "x9",  "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17",
                                          "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26",
-                                         "x27", "x28", "x29", "fp",  "lr",  "sp",  "pc",  "cpsr" };
+                                         "x27", "x28", "fp",  "lr",  "sp",  "pc",  "cpsr" };
 static const int g_registerNamesCount = sizeof(g_registerNames) / sizeof(*g_registerNames);
 
 static const char *g_exceptionRegisterNames[] = { "exception", "esr", "far" };
@@ -77,20 +77,20 @@ const char *kscpu_registerName(const int regNumber)
 
 uint64_t kscpu_registerValue(const KSMachineContext *const context, const int regNumber)
 {
-    if (regNumber <= 29) {
+    if (regNumber <= 28) {
         return context->machineContext.__ss.__x[regNumber];
     }
 
     switch (regNumber) {
-        case 30:
+        case 29:
             return context->machineContext.__ss.__fp;
-        case 31:
+        case 30:
             return context->machineContext.__ss.__lr;
-        case 32:
+        case 31:
             return context->machineContext.__ss.__sp;
-        case 33:
+        case 32:
             return context->machineContext.__ss.__pc;
-        case 34:
+        case 33:
             return context->machineContext.__ss.__cpsr;
         default:
             KSLOG_ERROR("Invalid register number: %d", regNumber);
