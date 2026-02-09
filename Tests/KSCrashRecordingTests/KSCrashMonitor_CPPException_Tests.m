8792e23e79e382ded3fe79c8ef69886622191fb1
Alexander Cohen <naftaly@users.noreply.github.com>
2026-02-09 14:59:44 -0500

    Add last_exception_backtrace to separate exception and handler backtraces
    For NSException and C++ exception crashes, the exception's origin backtrace
    (from callStackReturnAddresses or __cxa_throw) is now written to
    last_exception_backtrace, while the crashed thread shows the handler's
    actual call stack. This matches Apple's crash report format and fixes
    stale backtrace reuse in the C++ monitor via a thread-local validity flag.

---
+++ Tests/KSCrashRecordingTests/KSCrashMonitor_CPPException_Tests.m
@@ -0,0 +1,89 @@
+//
+//  KSCrashMonitor_CPPException_Tests.m
+//
+//  Created by Alexander Cohen on 2026-02-09.
+//
+//  Copyright (c) 2012 Karl Stenerud. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "KSCrashMonitor.h"
+#import "KSCrashMonitorContext.h"
+#import "KSCrashMonitor_CPPException.h"
+
+@interface KSCrashMonitor_CPPException_Tests : XCTestCase
+@end
+
+@implementation KSCrashMonitor_CPPException_Tests
+
+- (void)tearDown
+{
+    kscm_disableAllMonitors();
+    [super tearDown];
+}
+
+- (void)testInstallAndRemove
+{
+    KSCrashMonitorAPI *api = kscm_cppexception_getAPI();
+    api->setEnabled(true, NULL);
+    XCTAssertTrue(api->isEnabled(NULL));
+    api->setEnabled(false, NULL);
+    XCTAssertFalse(api->isEnabled(NULL));
+}
+
+- (void)testDoubleInstallAndRemove
+{
+    KSCrashMonitorAPI *api = kscm_cppexception_getAPI();
+
+    api->setEnabled(true, NULL);
+    XCTAssertTrue(api->isEnabled(NULL));
+    api->setEnabled(true, NULL);
+    XCTAssertTrue(api->isEnabled(NULL));
+
+    api->setEnabled(false, NULL);
+    XCTAssertFalse(api->isEnabled(NULL));
+    api->setEnabled(false, NULL);
+    XCTAssertFalse(api->isEnabled(NULL));
+}
+
+- (void)testMonitorId
+{
+    KSCrashMonitorAPI *api = kscm_cppexception_getAPI();
+    const char *monitorId = api->monitorId(NULL);
+    XCTAssertTrue(strcmp(monitorId, "CPPException") == 0, @"Expected monitorId 'CPPException', got '%s'", monitorId);
+}
+
+- (void)testEnableSwapCxaThrow
+{
+    KSCrashMonitorAPI *api = kscm_cppexception_getAPI();
+    api->setEnabled(true, NULL);
+
+    // Should not crash when called
+    kscm_enableSwapCxaThrow();
+
+    // Calling again should be idempotent
+    kscm_enableSwapCxaThrow();
+
+    api->setEnabled(false, NULL);
+}
+
+@end
