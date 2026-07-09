e62c374ab5c34fc1dfdbe13c6f082d7a6c84d83a
Mischan Toosarani-Hausberger <mischan@abovevacant.com>
2026-07-09 22:15:44 +0200

    Use effective monitor mask when registering monitors (#863)
    * Use effective monitor mask when registering monitors
    
    * format
    
    (cherry picked from commit f07b2b7e7680603a2bfb6306b107c1282c5fad0e)

---
+++ Tests/KSCrashRecordingTests/KSCrashMonitorSelection_Tests.m
@@ -0,0 +1,62 @@
+//
+//  KSCrashMonitorSelection_Tests.m
+//
+//  Created by Mischan Toosarani-Hausberger on 2026-07-09.
+//
+//  Copyright (c) 2012 Karl Stenerud. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "KSCrashMonitor.h"
+#import "KSCrashMonitorType.h"
+
+extern void kscm_testcode_resetState(void);
+extern void kscrash_testcode_setMonitors(KSCrashMonitorType monitorTypes);
+
+@interface KSCrashMonitorSelection_Tests : XCTestCase
+@end
+
+@implementation KSCrashMonitorSelection_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    kscm_testcode_resetState();
+}
+
+- (void)testCustomMonitorMaskRegistersRequiredInfrastructureMonitors
+{
+    kscrash_testcode_setMonitors(KSCrashMonitorTypeUserReported);
+
+    XCTAssertNotEqual(kscm_getMonitor("UserReported"), NULL);
+    XCTAssertNotEqual(kscm_getMonitor("System"), NULL);
+    XCTAssertNotEqual(kscm_getMonitor("Lifecycle"), NULL);
+    XCTAssertNotEqual(kscm_getMonitor("UserInfo"), NULL);
+    XCTAssertNotEqual(kscm_getMonitor("Resource"), NULL);
+
+    XCTAssertEqual(kscm_getMonitor("Signal"), NULL);
+    XCTAssertEqual(kscm_getMonitor("MachException"), NULL);
+    XCTAssertEqual(kscm_getMonitor("NSException"), NULL);
+    XCTAssertEqual(kscm_getMonitor("Zombie"), NULL);
+}
+
+@end
